Got rid of the lsat relu on the models, fixes exploding gradient

ucb_rl2_meta/model.py
@@ -184,7 +184,7 @@ class TransitionModel(nn.Module):
         layers.append(Conv2d_tf(16, 16, kernel_size=kernel_size, stride=1, padding=(conv_padding,conv_padding)))
         layers.append(nn.ReLU(inplace=True))
         layers.append(Conv2d_tf(16, 1, kernel_size=kernel_size, stride=1, padding=(conv_padding,conv_padding)))
-        layers.append(nn.ReLU(inplace=True))
+        # layers.append(nn.ReLU(inplace=True))
 
         self.model = nn.Sequential(*layers)
 
@@ -207,7 +207,7 @@ class RewardModel(nn.Module):
         layers.append(Conv2d_tf(16, 16, kernel_size=kernel_size, stride=1, padding=(conv_padding,conv_padding)))
         layers.append(nn.ReLU(inplace=True))
         layers.append(Conv2d_tf(16, 1, kernel_size=kernel_size, stride=1, padding=(conv_padding,conv_padding)))
-        layers.append(nn.ReLU(inplace=True))
+        # layers.append(nn.ReLU(inplace=True))
 
         self.model = nn.Sequential(*layers)
 