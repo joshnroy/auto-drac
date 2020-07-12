bb917852ad79ee39eb46ba6bacf0a4bc67221825
Josh Roy <joshnroy@gmail.com>
2020-07-12 18:05:41 -0400

    separated the actor into a different optimizer

--- ucb_rl2_meta/algo/conv_drac.py
+++ ucb_rl2_meta/algo/conv_drac.py
@@ -35,10 +35,12 @@ class ConvDrAC():
 
         self.max_grad_norm = max_grad_norm
 
-        self.actor_critic_parameters = list(actor_critic.base.parameters()) + list(actor_critic.dist.parameters())
+        self.critic_parameters = list(actor_critic.base.parameters())
+        self.actor_parameters = list(actor_critic.dist.parameters())
         self.model_parameters = list(actor_critic.transition_model.parameters()) + list(actor_critic.reward_model.parameters()) + list(actor_critic.base.layer1.parameters()) + list(actor_critic.base.layer2.parameters()) + list(actor_critic.base.layer3.parameters()) + list(actor_critic.base.fc.parameters())
 
-        self.optimizer = optim.Adam(self.actor_critic_parameters, lr=lr, eps=eps)
+        self.optimizer_critic = optim.Adam(self.critic_parameters, lr=lr, eps=eps)
+        self.optimizer_actor = optim.Adam(self.actor_parameters, lr=lr, eps=eps)
         self.optimizer_model = optim.Adam(self.model_parameters, lr=lr, eps=eps)
         
         self.aug_id = aug_id
@@ -72,26 +74,41 @@ class ConvDrAC():
                 value_preds_batch, return_batch, masks_batch, old_action_log_probs_batch, \
                         adv_targ, next_obs_batch = sample
 
+# MODEL OPTIMIZATION
                 with torch.no_grad():
                     _, next_obs_features, _ = self.actor_critic.base(next_obs_batch, recurrent_hidden_states_batch, masks_batch)
                     next_obs_features = torch.reshape(next_obs_features, (-1, 1) + self.actor_critic.state_shape)
                 
+                predicted_next_states, predicted_rewards = self.actor_critic.predict_next_state_reward(obs_batch, recurrent_hidden_states_batch, masks_batch, actions_batch)
+
+                next_state_loss = F.mse_loss(predicted_next_states, next_obs_features)
+                reward_loss = F.mse_loss(predicted_rewards, predicted_rewards)
+                model_loss = next_state_loss + reward_loss
+
+                self.optimizer_model.zero_grad()
+                (model_loss * self.model_coef).backward()
+                self.optimizer_model.step()  
+
+# -------------------
+
+# ACTION AND VALUE OPTIMIZATION
+
                 values, action_log_probs, dist_entropy, _ = self.actor_critic.evaluate_actions(
                     obs_batch, recurrent_hidden_states_batch, masks_batch,
                     actions_batch)
-
-                predicted_next_states, predicted_rewards = self.actor_critic.predict_next_state_reward(obs_batch, recurrent_hidden_states_batch, masks_batch, actions_batch)
-
-                next_state_loss = torch.mean((next_obs_features - predicted_next_states)**2.)
-                reward_loss = torch.mean((return_batch - predicted_rewards)**2.)
-                model_loss = next_state_loss + reward_loss
-
+                                
                 ratio = torch.exp(action_log_probs -
                                   old_action_log_probs_batch)
                 surr1 = ratio * adv_targ
                 surr2 = torch.clamp(ratio, 1.0 - self.clip_param,
                                     1.0 + self.clip_param) * adv_targ
                 action_loss = -torch.min(surr1, surr2).mean()
+
+                self.optimizer_actor.zero_grad()
+                (action_loss - dist_entropy * self.entropy_coef).backward(retain_graph=True)
+                nn.utils.clip_grad_norm_(self.actor_parameters,
+                                        self.max_grad_norm)
+                self.optimizer_actor.step()
 
                 value_pred_clipped = value_preds_batch + \
                     (values - value_preds_batch).clamp(-self.clip_param, self.clip_param)
@@ -100,6 +117,15 @@ class ConvDrAC():
                     value_pred_clipped - return_batch).pow(2)
                 value_loss = 0.5 * torch.max(value_losses,
                                                 value_losses_clipped).mean()
+
+                self.optimizer_critic.zero_grad()
+                (value_loss * self.value_loss_coef).backward()
+                nn.utils.clip_grad_norm_(self.critic_parameters,
+                                        self.max_grad_norm)
+                self.optimizer_critic.step()
+
+# -------------------
+
                 if False:
                     obs_batch_aug = self.aug_func.do_augmentation(obs_batch)
                     obs_batch_id = self.aug_id(obs_batch)
@@ -117,25 +143,22 @@ class ConvDrAC():
                     value_loss_aug = 0.
 
                 # Update actor-critic using both PPO and Augmented Loss. Also update model using model loss
-                self.optimizer.zero_grad()
-                self.optimizer_model.zero_grad()
-                aug_loss = value_loss_aug + action_loss_aug
-                (value_loss * self.value_loss_coef + action_loss -
-                    dist_entropy * self.entropy_coef + 
-                    aug_loss * self.aug_coef + model_loss * self.model_coef).backward()
-                nn.utils.clip_grad_norm_(self.actor_critic_parameters,
-                                        self.max_grad_norm)
-                self.optimizer.step()  
-                self.optimizer_model.step()  
-                                
+                # aug_loss = value_loss_aug + action_loss_aug
+                # (value_loss * self.value_loss_coef + action_loss -
+                #     dist_entropy * self.entropy_coef + 
+                #     aug_loss * self.aug_coef + model_loss * self.model_coef).backward()
+                # nn.utils.clip_grad_norm_(self.critic_parameters,
+                #                         self.max_grad_norm)
+
+
                 value_loss_epoch += value_loss.item()
                 action_loss_epoch += action_loss.item()
                 dist_entropy_epoch += dist_entropy.item()
                 transition_model_loss_epoch += next_state_loss.item()
                 reward_model_loss_epoch += reward_loss.item()
 
-                if self.aug_func:
-                    self.aug_func.change_randomization_params_all()
+                # if self.aug_func:
+                #     self.aug_func.change_randomization_params_all()
 
         num_updates = self.ppo_epoch * self.num_mini_batch
 
