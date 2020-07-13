6c54738ab2d3d74870be37fd73e6c15fb43293e9
Josh Roy <joshnroy@gmail.com>
2020-07-13 16:05:33 -0400

    plots against ppo, plots model losses

--- plot_results.py
+++ plot_results.py
@@ -15,12 +15,15 @@ matplotlib.use("Agg")
 
 sns.set_style("darkgrid")
 
+name = "separate_optimizers2_modelbased_logs/"
+
 env_names = ["bigfish", "bossfight", "caveflyer", "chaser", "climber", "coinrun", "dodgeball", 
              "fruitbot", "heist", "jumper", "leaper", "maze", "miner", "ninja", "plunder", "starpilot"]
 num_wide = int(np.sqrt(len(env_names)))
 fig, axes = plt.subplots(nrows=num_wide, ncols=num_wide, figsize=(20, 20))
 for i_env, env_name in tqdm(enumerate(env_names), total=len(env_names)):
-    file_naming_scheme="new_modelbased_logs/" + env_name + "/" + env_name + "-*/progress-drac-" + env_name + "-reproduce-s1.csv"
+    file_naming_scheme=name + env_name + "/" + env_name + "-*/progress-drac-" + env_name + "-reproduce-s1.csv"
+    original_file_naming_scheme="original_ppo_logs/" + env_name + "/" + env_name + "-*/progress-drac-" + env_name + "-reproduce-s1.csv"
     big_df = pd.DataFrame()
     ax = axes[i_env // num_wide, i_env % num_wide]
 #     ax = axes
@@ -28,7 +31,17 @@ for i_env, env_name in tqdm(enumerate(env_names), total=len(env_names)):
         try:
             df = pd.read_csv(file_name)
             df = df[["train/mean_episode_reward", "test/mean_episode_reward", "train/total_num_steps"]]
-            df.columns = ["Train", "Test", "Timestep"]
+            df.columns = ["Model Train", "Model Test", "Timestep"]
+            df.loc[:, df.columns != "Timestep"] = df.loc[:, df.columns != "Timestep"].rolling(window=20).mean()
+            df = pd.melt(df, id_vars=['Timestep'], var_name="Type", value_name="Reward")
+            big_df = big_df.append(df, ignore_index=True)
+        except Exception as e:
+            print(file_name, e)
+    for file_name in glob(original_file_naming_scheme):
+        try:
+            df = pd.read_csv(file_name)
+            df = df[["train/mean_episode_reward", "test/mean_episode_reward", "train/total_num_steps"]]
+            df.columns = ["PPO Train", "PPO Test", "Timestep"]
             df.loc[:, df.columns != "Timestep"] = df.loc[:, df.columns != "Timestep"].rolling(window=20).mean()
             df = pd.melt(df, id_vars=['Timestep'], var_name="Type", value_name="Reward")
             big_df = big_df.append(df, ignore_index=True)
@@ -38,4 +51,27 @@ for i_env, env_name in tqdm(enumerate(env_names), total=len(env_names)):
         sns.lineplot(data=big_df, x="Timestep", y="Reward", hue="Type", ci='sd', ax=ax)
     ax.set_title(env_name.title())
 
-plt.savefig("out.png")
+plt.savefig("rewards.png")
+
+fig, axes = plt.subplots(nrows=num_wide, ncols=num_wide, figsize=(20, 20))
+for i_env, env_name in tqdm(enumerate(env_names), total=len(env_names)):
+    file_naming_scheme=name + env_name + "/" + env_name + "-*/progress-drac-" + env_name + "-reproduce-s1.csv"
+    big_df = pd.DataFrame()
+    ax = axes[i_env // num_wide, i_env % num_wide]
+#     ax = axes
+    for file_name in glob(file_naming_scheme):
+        try:
+            df = pd.read_csv(file_name)
+            df = df[["losses/transition_model_loss", "losses/reward_model_loss", "train/total_num_steps"]]
+            df.columns = ["Transition Model", "Reward Model", "Timestep"]
+            df.loc[:, df.columns != "Timestep"] = np.sqrt(df.loc[:, df.columns != "Timestep"]).clip(0, 4.).rolling(window=1).mean()
+            df = pd.melt(df, id_vars=['Timestep'], var_name="Type", value_name="Value")
+            big_df = big_df.append(df, ignore_index=True)
+        except Exception as e:
+            print(file_name, e)
+    if len(big_df) > 0:
+        sns.lineplot(data=big_df, x="Timestep", y="Value", hue="Type", ci='sd', ax=ax)
+    ax.set_title(env_name.title())
+
+plt.savefig("model_losses.png")
+plt.close()
